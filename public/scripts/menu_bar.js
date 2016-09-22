ffbe1758185de6fb64f6f9b102d96d3cf71c621f
Michael Dandy <mdandy1988@gmail.com>
2016-09-21 21:33:48 -0700

    Make menu bar interactive

--- public/scripts/menu_bar.js
+++ public/scripts/menu_bar.js
@@ -2,7 +2,7 @@ var MenuBar = React.createClass({
 
   cssClass: "pantler-menu-bar",
 
-  _getMenuItems: function() {
+  getMenuItems: function() {
     return [{
       id: 0,
       label: "Pantry"
@@ -12,21 +12,29 @@ var MenuBar = React.createClass({
     }];
   },
 
+  activateMenuItem: function(index) {
+    this.setState({
+      selectedIndex: index
+    });
+  },
+
   getInitialState: function() {
     return {
-      menuItems: this._getMenuItems()
+      menuItems: this.getMenuItems(),
+      selectedIndex: 0,
     };
   },
 
   render: function() {
+    var self = this;
     return (
       <div className={this.cssClass + " slds-tabs--default"}>
         <ul className="slds-tabs--default__nav">
           {
-            this.state.menuItems.map(function(menuItem) {
+            this.state.menuItems.map(function(menuItem, index) {
               return (
-                <li className="slds-tabs--default__item slds-text-heading--label" key={menuItem.id}>
-                  <a className="slds-tabs--default__link" href="javascript:void(0);">{menuItem.label}</a>
+                <li className={"slds-tabs--default__item slds-text-heading--label" + (index === self.state.selectedIndex ? " selected" : "")} key={menuItem.id}>
+                  <a onClick={self.activateMenuItem.bind(self, index)} className="slds-tabs--default__link" href="javascript:void(0);">{menuItem.label}</a>
                 </li>
               )
             })
